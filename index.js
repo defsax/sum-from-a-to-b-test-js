1704faef01fe7b500438c06281695da32c7c75d1
Perry Defayette <defayetteperry@gmail.com>
2021-01-08 23:58:02 +0000

    Completed challenge.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 
-function sum(fromN, toN) {
+const sum = function(fromN, toN) {
   // Sum all the values from fromN up to toN
-}
+  let total = 0;
+  if (fromN < toN) {
+    total = sum(fromN + 1, toN);
+  }
+  return total + fromN;
+};
 
 module.exports = sum;
